036f4236223c5375cf4461235564852929f9a80a
Siddharth Arora <siddharth@dripcapital.com>
2020-03-23 16:57:00 +0530

    final

--- src/components/layout/MainFooter/MainFooter.vue
+++ src/components/layout/MainFooter/MainFooter.vue
@@ -48,7 +48,7 @@ export default {
        */
     copyright: {
       type: String,
-      default: 'Copyright © 2018 DesignRevision',
+      default: 'Copyright © 2020 ARORA INC.',
     },
     /**
        * Whether the footer should be wrapped in a container, or not.
